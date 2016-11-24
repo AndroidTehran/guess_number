0c226a94eeaef6691077058797e68e1514b28aca
Mohammad <m.nourinik@irantalent.com>
2016-11-24 18:19:49 +0330

    نوشتن کد دکمه 1 و دکمه حدس عدد

--- app/src/main/java/com/pouya11/guessnumber/GuessActivity.java
+++ app/src/main/java/com/pouya11/guessnumber/GuessActivity.java
@@ -2,12 +2,41 @@ package com.pouya11.guessnumber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class GuessActivity extends AppCompatActivity {
 
+    EditText txtGuess;
+    int number;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guess);
+
+        txtGuess = (EditText) findViewById(R.id.txt_guess);
+
+        Random r = new Random();
+
+        number = r.nextInt(9000) + 1000;
+    }
+
+    public void btn1Clicked(View view) {
+        txtGuess.append("1");
+        txtGuess.setText(txtGuess.getText().toString() + "1");
+    }
+
+    public void btnGuessClicked(View view) {
+        int guessNumber = Integer.parseInt(txtGuess.getText().toString());
+        if(guessNumber == number){
+            Toast.makeText(this, "You win", Toast.LENGTH_LONG).show();
+        }else if(guessNumber > number){
+            Toast.makeText(this, "Too high!!!", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this, "You low :|", Toast.LENGTH_LONG).show();
+        }
     }
 }
